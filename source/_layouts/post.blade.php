63d055bc9a71a3fdac5341c2871b3d39ceeaeb8e
Bayu Hendra Winata <uyab.exe@gmail.com>
2019-10-17 15:13:38 +0700

    styling

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -1,10 +1,10 @@
 @extends('_layouts.master')
 
 @push('meta')
-    <meta property="og:title" content="{{ $page->title }}" />
-    <meta property="og:type" content="article" />
+    <meta property="og:title" content="{{ $page->title }}"/>
+    <meta property="og:type" content="article"/>
     <meta property="og:url" content="{{ $page->getUrl() }}"/>
-    <meta property="og:description" content="{{ $page->description }}" />
+    <meta property="og:description" content="{{ $page->description }}"/>
 @endpush
 
 @section('body')
@@ -14,41 +14,44 @@
 
     <h1 class="leading-none mb-2">{{ $page->title }}</h1>
 
-    <p class="text-gray-600 text-base md:my-0">Ditulis oleh {{ $page->author }}  pada  {{ date('j F Y', $page->date) }}</p>
+    <p class="text-gray-600 text-base md:my-0">Ditulis oleh {{ $page->author }}
+        pada {{ date('j F Y', $page->date) }}</p>
 
-{{--    @if ($page->categories)--}}
-{{--        <div class="mb-10 mt-4">--}}
-{{--            @foreach ($page->categories as $i => $category)--}}
-{{--                <a--}}
-{{--                        href="{{ '/post/categories/' . $category }}"--}}
-{{--                        title="View posts in {{ $category }}"--}}
-{{--                        class="inline-block bg-gray-200 font-bold hover:bg-red-200 leading-loose tracking-wide text-gray-800 uppercase text-xs rounded mr-1 px-3 pt-px"--}}
-{{--                >{{ $category }}</a>--}}
-{{--            @endforeach--}}
-{{--        </div>--}}
-{{--    @endif--}}
+    {{--    @if ($page->categories)--}}
+    {{--        <div class="mb-10 mt-4">--}}
+    {{--            @foreach ($page->categories as $i => $category)--}}
+    {{--                <a--}}
+    {{--                        href="{{ '/post/categories/' . $category }}"--}}
+    {{--                        title="View posts in {{ $category }}"--}}
+    {{--                        class="inline-block bg-gray-200 font-bold hover:bg-red-200 leading-loose tracking-wide text-gray-800 uppercase text-xs rounded mr-1 px-3 pt-px"--}}
+    {{--                >{{ $category }}</a>--}}
+    {{--            @endforeach--}}
+    {{--        </div>--}}
+    {{--    @endif--}}
 
     <hr>
 
-    <div class="border-b border-red-200 mb-10 pb-4 markdown" v-pre>
+    <div class="mb-10 pb-4 markdown" v-pre>
         {!! $page->content !!}
     </div>
 
-    <nav class="flex justify-between text-sm md:text-base">
-        <div>
+    <nav class="text-sm border-t-2 py-8 text-center">
+        Mungkin Anda tertarik dengan artikel menarik lainya:
+        <ol>
             @if ($next = $page->getNext())
-                <a href="{{ $next->getUrl() }}" title="Older Post: {{ $next->title }}">
-                    &LeftArrow; {{ $next->title }}
-                </a>
+                <li>
+                    <a href="{{ $next->getUrl() }}" title="Older Post: {{ $next->title }}">
+                        {{ $next->title }}
+                    </a>
+                </li>
             @endif
-        </div>
-
-        <div>
             @if ($previous = $page->getPrevious())
-                <a href="{{ $previous->getUrl() }}" title="Newer Post: {{ $previous->title }}">
-                    {{ $previous->title }} &RightArrow;
-                </a>
+                <li>
+                    <a href="{{ $previous->getUrl() }}" title="Newer Post: {{ $previous->title }}">
+                        {{ $previous->title }}
+                    </a>
+                </li>
             @endif
-        </div>
+        </ol>
     </nav>
 @endsection
